266a651443022be4865ec2d4b12dada75a9d62fa
Martin Schoen <schoenm@cip.ifi.lmu.de>
2018-02-28 01:04:13 +0100

    Implement design for counter

--- home-page/slider-section.php
+++ home-page/slider-section.php
@@ -16,27 +16,34 @@ if(!empty($slider_page_id_1) || !empty($slider_page_id_2) || !empty($slider_page
     if($query->have_posts()): ?>
         <div class="home-banner">
             <div class="slider-item-wrapper" <?php if(is_rtl()){?>data-slick='{"rtl":true}'<?php }?>>
-              <?php while($query->have_posts()) : $query->the_post();
-                          $slider_image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );?>
-                          <div class="slider-item" style="background-image: url(<?php echo esc_url($slider_image[0]); ?>)">
-                              <div class="slider-text-wrapper">
-                                  <div class="block">
-                                    <div class="centre">
-                                      <ul class="bars active">
-                                        <li class="bar1"></li>
-                                        <li class="bar2"></li>
-                                        <li class="bar3"></li>
-                                        <li class="bar4"></li>
-                                        <li class="bar5"></li>
-                                      </ul>
-                                    </div>
-                                  </div>
-                                  <?php the_title( '<h2>', '</h2>' ); ?>
-                                  <a href="<?php the_permalink() ?>" class="sl-btn"><?php esc_html_e('Weiterlesen','sungit-lite')?></a>
-                              </div>
-                          </div>
-              <?php endwhile;
-              wp_reset_postdata(); ?>
+                <?php while($query->have_posts()) : $query->the_post();
+                    $slider_image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );?>
+                    <div class="slider-item" style="background-image: url(<?php echo esc_url($slider_image[0]); ?>)">
+                        <div class="slider-text-wrapper">
+                            <div class="block">
+                            <div class="centre">
+                                <ul class="bars active">
+                                    <li class="bar1"></li>
+                                    <li class="bar2"></li>
+                                    <li class="bar3"></li>
+                                    <li class="bar4"></li>
+                                    <li class="bar5"></li>
+                                </ul>
+                            </div>
+                            </div>
+                            <?php the_title( '<h2>', '</h2>' ); ?>
+                            <div class="sk-counter">
+                                <p class="sk-counter-overline">Noch</p>
+                                <p class="sk-counter-display">
+                                    <span id="sk-counter-days">61</span> Tage und
+                                    <span id="sk-counter-hrs">23</span>:<span id="sk-counter-min">17</span>:<span id="sk-counter-sec">24</span> Stunden
+                                </p>
+                                <p class="sk-counter-underline">bis zum Startschuss</p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile;
+                wp_reset_postdata(); ?>
             </div>
         </div>
     <?php endif;
